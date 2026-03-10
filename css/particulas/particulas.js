a35cf855897f972146f34d8edc123f89b10cb350
jerrySacort <jerry.dc37@gmail.com>
2026-03-10 12:10:29 -0600

    letras en color dorado

--- css/particulas/particulas.js
+++ css/particulas/particulas.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-/*document.querySelectorAll('.corazon').forEach(elemento => {
-  elemento.addEventListener('click', () => {
-    const contenedor = document.querySelector('.contenedorParticulas');
-    let particulas = '';
-    for (let i = 0; i < 200; i++) {
-      particulas += '<div class="circle-container"><div class="circle"></div></div>';
-    }
-    contenedor.insertAdjacentHTML('beforeend', particulas);
-  });
-});*/
-
 document.querySelectorAll('.corazon').forEach(elemento => {
   elemento.addEventListener('click', () => {
     const contenedor = document.querySelector('.contenedorParticulas1');
@@ -26,33 +14,4 @@ document.querySelectorAll('.corazon').forEach(elemento => {
     }
     contenedor.appendChild(fragment);
   });
-=======
-/*document.querySelectorAll('.corazon').forEach(elemento => {
-  elemento.addEventListener('click', () => {
-    const contenedor = document.querySelector('.contenedorParticulas');
-    let particulas = '';
-    for (let i = 0; i < 200; i++) {
-      particulas += '<div class="circle-container"><div class="circle"></div></div>';
-    }
-    contenedor.insertAdjacentHTML('beforeend', particulas);
-  });
-});*/
-
-document.querySelectorAll('.corazon').forEach(elemento => {
-  elemento.addEventListener('click', () => {
-    const contenedor = document.querySelector('.contenedorParticulas1');
-    contenedor.classList.remove("contenedorParticulas1")
-    contenedor.classList.add("contenedorParticulas");
-    const fragment = document.createDocumentFragment();
-    for (let i = 0; i < 200; i++) {
-      const container = document.createElement('div');
-      container.className = 'circle-container';
-      const circle = document.createElement('div');
-      circle.className = 'circle';
-      container.appendChild(circle);
-      fragment.appendChild(container);
-    }
-    contenedor.appendChild(fragment);
-  });
->>>>>>> 8b855e76e099d697b7ccaf52badb2d87cf0391cc
 });
